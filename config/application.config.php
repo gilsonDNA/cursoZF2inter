393bfc67476e346d985576a701ba1e37ad4d7321
Gilson <gilson@idnadevendas.com.br>
2015-12-01 14:03:49 -0200

    Projeto cursoZF2inter

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,11 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'DoctrineModule',
+        'DoctrineORMModule',
+        //'DoctrineDataFixtureModule',
+        'SONBase',
+        'SONUser',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
